ad0fbc7850a779ce41229569094ab4bcec388e87
Murat Senel <msenel@gmail.com>
2020-08-05 21:33:03 +0000

    grahnode: Avaoid copy contructor via std::move

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -38,13 +38,13 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 ////
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
-    _chatBot = chatbot;
+    _chatBot = std::move(chatbot);
     _chatBot.SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(_chatBot);
+    newNode->MoveChatbotHere(std::move(_chatBot));
 
     // Task 5: Not needed anymore
     //_chatBot = nullptr; // invalidate pointer at source
